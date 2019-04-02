87bbcd69e9e84071f99cfd680dd055e8d14e56c5
gushiyu <923686643@qq.com>
2019-04-02 09:55:13 +0800

    Initial commit

---
+++ src/main/java/com/gsy/security/config/MySecurityConfig.java
@@ -0,0 +1,40 @@
+package com.gsy.security.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+
+        //开启自动配置登录功能
+        http.formLogin().loginPage("/userlogin");
+        //开启自动配置的注销功能
+        http.logout().logoutSuccessUrl("/");
+
+        //开启记住我
+        http.rememberMe().rememberMeParameter("remember");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("zhangsan").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP1")
+                .and()
+                .withUser("lisi").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP2")
+                .and()
+                .withUser("wangwu").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP3");
+    }
+}
